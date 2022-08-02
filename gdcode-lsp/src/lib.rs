40311dbf153b850f4969b679d62f9d7d642d7474
megaredb <mredikf@gmail.com>
2022-08-02 18:15:28 +0300

    Added build_client to gdcode-lsp

--- gdcode-lsp/src/lib.rs
+++ gdcode-lsp/src/lib.rs
@@ -1,8 +1,13 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use jsonrpsee_ws_client::{WsClientBuilder, HeaderMap, HeaderValue, WsClient};
+
+#[tokio::main]
+async fn build_client(host: &str, port: &i32) -> WsClient {
+    let mut headers = HeaderMap::new();
+    headers.insert("Any-Header-You-Like", HeaderValue::from_static("42"));
+
+    WsClientBuilder::default()
+    .set_headers(headers)
+    .build(format!("ws://{host}:{port}").as_str())
+    .await
+    .unwrap()
 }
